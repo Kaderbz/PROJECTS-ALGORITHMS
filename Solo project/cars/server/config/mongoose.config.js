53134ab5c10ef0ed2b4c83d659dbb1210d5e72b6
kaderbz <bouzouita.abdelkader@gmail.com>
2024-05-15 00:12:21 +0100

    Update mongoose.config.js

--- Solo project/cars/server/config/mongoose.config.js
+++ Solo project/cars/server/config/mongoose.config.js
@@ -1,18 +1,8 @@
 const mongoose = require('mongoose');
 
-const DB = "assistanceDB"
+const DB = process.env.DB
 
 mongoose.connect(`mongodb://127.0.0.1:27017/${DB}`)
 
 .then(()=>console.log(`Connected to ${DB} database!`))
-.catch((err)=>console.log(err));          
-
-/*
-
-const mongoose = require("mongoose")
-
-const dbName = process.env.DB
-
-mongoose.connect(`mongodb://127.0.0.1:3000/${dbName}`)
-    .then(() => console.log(`📡📡📡 Established a connection to the ${dbName} database`))
-    .catch(err => console.log("❌❌❌❌ Something went wrong when connecting to the database", err));*/
+.catch((err)=>console.log(err));
